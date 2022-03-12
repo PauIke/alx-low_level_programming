f52d77c5b0c21ba34280f548c4cb757756ec176e
PauIke <paunyoike@gmail.com>
2022-03-12 09:30:46 -0800

    Corrected code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual puzzle, folowed by 		a new line
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
